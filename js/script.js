79d1a3cc9718e1bef55f5e77a1d7b56ad84d92b4
George Flores <georgeflores886@gmail.com>
2020-10-09 15:01:37 -0400

    added playSound function and keycode events

--- js/script.js
+++ js/script.js
@@ -1,8 +1,18 @@
 const playSound = e => {
-    const audio = (`audio[data-key="${e.keyCode}]"`);
-    const key = $(`.key[data-key="${e.keyCode}]"`)
-    if (!audio) return;
-    audio.currentTime = 0;
-    audio.play();
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
 
+    if (!key) return;
+    key.classList.add('playing')
+    audio.currentTime = 0;
+    audio.play()
 }
+
+const removeTransition = e => {
+    if (e.propertyName !== 'transform') return;
+    this.classList.remove('playing')
+}
+
+const keys = document.querySelectorAll('.key')
+keys.forEach(key => key.addEventListener('transitioned', removeTransition))
+window.addEventListener('keydown', playSound)
