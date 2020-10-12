92eceeaea19c22efa5bcb43f4e946b300cd886cc
George Flores <georgeflores886@gmail.com>
2020-10-12 13:13:20 -0400

    created guitar sounds and page

--- js/script.js
+++ js/script.js
@@ -2,10 +2,11 @@ const playSound = e => {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
 
-    if (!key) return;
-    key.classList.add('playing')
+    if (!audio) return;
+
     audio.currentTime = 0;
     audio.play()
+    key.classList.add('playing')
 }
 
 const removeTransition = e => {
